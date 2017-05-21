5a00c81bea6f76ba6d2f7f6d2fef55be868c1ed4
David Batranu <david.batranu@eaudeweb.ro>
2017-05-21 00:10:34 +0000

    Fixing patch. Broken during merge.

--- patches/00217.sql
+++ patches/00217.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 CREATE TABLE Visual3D (
   id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   path VARCHAR(255) NOT NULL,
@@ -16,8 +15,3 @@ CREATE TABLE VisualTag3D (
   createDate INT(11),
   modDate INT(11)
 ) DEFAULT CHARACTER SET "utf8" ENGINE=InnoDB;
-
-=======
-alter table Entry add key(structStatus);
-alter table Source add key(structurable);
->>>>>>> master
